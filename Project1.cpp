94bf7fcc193bd01e69d0f761c3aeb9235acfc110
ivanbaydov <59543077+ivanbaydov@users.noreply.github.com>
2021-10-24 17:08:08 -0700

    Add files via upload

---
+++ Project1.cpp
@@ -0,0 +1,49 @@
+// Project1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+//#include <iostream>
+
+//package Week_8;
+
+
+//public class Savings_Account_Class {
+   // private static double annualInterestRate = 0;
+   // private double savingsBalance;
+
+    //public Savings_Account_Class(double Bal) {
+    //    savingsBalance = Bal;
+    //}
+   // public void calculateMonthlyInterest() {
+      //  savingsBalance += savingsBalance * (annualInterestRate / 12.0);
+   // }
+   // public static void modifyInterstRate(double Rate) {
+     //   annualInterestRate = (Rate >= 0 && Rate <= 1.0) ? Rate : 0.04;
+    //}
+   // public String toString() {
+       // return String.format("$%.2f", savingsBalance);
+   // }
+  //  public static void main(String[] args) {
+      //  Savings_Account_Class.modifyInterstRate(0.04);
+       // Savings_Account_Class Saver1 = new Savings_Account_Class(2000);
+       // Savings_Account_Class Saver2 = new Savings_Account_Class(3000);
+       // System.out.println("Enter your monthy balance for on year at .04");
+       // System.out.println("The Balances: ");
+
+      // System.out.printf("%20s%10s\n", "Saver 1", "Saver 2");
+       // System.out.printf("%-10s%10s%10s\n", "Base", Saver1.toString(), Saver2.toString());
+       // for (int i = 1; i <= 12; i++) {
+       //     String Month = String.format("Month %d:", i);
+       //     Saver1.calculateMonthlyInterest();
+       //     Saver2.calculateMonthlyInterest();
+       //     System.out.printf("%-10s%10s%10s\n", Month, Saver1.toString(), Saver2.toString());
+       // }
+      //  Savings_Account_Class.modifyInterstRate(0.05);
+       // Saver1.calculateMonthlyInterest();
+       // Saver2.calculateMonthlyInterest();
+       // System.out.println("After changing to intrest rate 0.5");
+       // System.out.println("Balances:");
+       // System.out.printf("%-10s%10s\n", "Saver 1", "Saver 2");
+       // System.out.printf("%-10s%10s\n", Saver1.toString(), Saver2.toString());
+
+    //}
+//}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Project1
+ This is my java course example code i did for one of my projects but commented on visual studio.
